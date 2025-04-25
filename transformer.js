89397d724b5eda73a19fe83811f782c78efa8509
Dimas <78547184+dimasab@users.noreply.github.com>
2025-04-25 18:08:44 +0700

    Update transformer.js

--- transformer.js
+++ transformer.js
@@ -1,1 +1,26 @@
-wkwkkwkkwkwkkkwkw
+const autobots = [
+    "Optimus Prime",
+    "Bumblebee",
+    "Ratchet",
+    "Ironhide",
+    "Jazz",
+    "Prowl",
+    "Wheeljack",
+    "Arcee",
+    "Windblade"
+];
+const decepticons = [
+    "Megatron",
+    "Starscream",
+    "Soundwave",
+    "Shockwave",
+    "Thundercracker",
+    "Skywarp",
+    "Blitzwing",
+    "Bonecrusher",
+    "Blackout"
+];
+function printTransformers() {
+    console.log("wkkwkkkwkkkwkwkkw");
+}
+printTransformers();
